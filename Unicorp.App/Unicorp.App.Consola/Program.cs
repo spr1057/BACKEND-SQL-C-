4334316a0317b7a09c2f5570a9e527a3ec2d160b
YesikaMPG <annyka96@hotmail.com>
2022-09-23 20:46:16 -0500

    PRUEBA YESI

--- Unicorp.App/Unicorp.App.Consola/Program.cs
+++ Unicorp.App/Unicorp.App.Consola/Program.cs
@@ -7,7 +7,7 @@ namespace Unicorp.App.Consola
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Profe no se, eso no me da!");
+            
         }
     }
 }
